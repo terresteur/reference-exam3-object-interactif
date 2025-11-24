bb96c60a31d0ba86d69e0ed4bbd96e9ef6f05ac7
terresteur <112128653+terresteur@users.noreply.github.com>
2025-11-24 14:45:21 -0500

    commentaire

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,29 @@ MicroOscSlip<128> monOsc(& Serial);
 #define CANAL_KEY_UNIT_BOUTON 1
 #define CANAL_KEY_UNIT_ANGLE 2  
 
-unsigned long monChronoDepart ;
-M5_PbHub myPbHub;
+unsigned long monChronoDepart ; //Utilisée pour savoir quand 20 ms sont passées dans la loop.
+
+M5_PbHub myPbHub; //Permet d’utiliser toutes les fonctions du hub → digitalRead, analogRead, pixels…
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(115200); //communication série (nécessaire pour OSC).
 
-  Wire.begin();
-  myPbHub.begin();
+  Wire.begin(); //initialise I2C, obligatoire pour le PbHub.
+  myPbHub.begin(); //démarre le PbHub
 
-  myPbHub.setPixelCount( CANAL_KEY_UNIT_BOUTON , 1);
+  myPbHub.setPixelCount( CANAL_KEY_UNIT_BOUTON , 1); //dit combien de pixels LED tu veux sur ce canal.
 
 }
 
+//chaque fois qu’un message OSC arrive, cette fonction est appelée et reçoit le message à traiter.
 void myOscMessageParser(MicroOscMessage & receivedOscMessage) {
   // Ici, un if et receivedOscMessage.checkOscAddress() est utilisé pour traiter les différents messages
-  if (receivedOscMessage.checkOscAddress("/pixel")) {  // MODIFIER /pixel pour l'adresse qui sera reçue
+  if (receivedOscMessage.checkOscAddress("/pixel")) {  // MODIFIER /pixel pour l'adresse qui sera reçue et Si c’est bien "/pixel", on entre dans ce bloc if
        int premierArgument = receivedOscMessage.nextAsInt(); // Récupérer le premier argument du message en tant que int
        int deuxiemerArgument = receivedOscMessage.nextAsInt(); // SI NÉCESSAIRE, récupérer un autre int
        int troisiemerArgument = receivedOscMessage.nextAsInt(); // SI NÉCESSAIRE, récupérer un autre int
-       myPbHub.setPixelColor( CANAL_KEY_UNIT_BOUTON , 0 , premierArgument,deuxiemerArgument,troisiemerArgument );
+       myPbHub.setPixelColor( CANAL_KEY_UNIT_BOUTON , 0 , premierArgument,deuxiemerArgument,troisiemerArgument ); // 0 → numéro de la LED dans ce canal (souvent 0 si tu n’as qu’une LED).
        // UTILISER ici les arguments récupérés
  
    // SI NÉCESSAIRE, ajouter d'autres if pour recevoir des messages avec d'autres adresses
@@ -50,7 +52,6 @@ void loop() {
     //int rotation = maLectureAngle * 360 / 4095;
     monOsc.sendInt("/angle", maLectureAngle);
 }
-
 
 }
 
